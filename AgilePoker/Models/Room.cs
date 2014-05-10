5eab8d26d4d4aa243202b40a6f9fe63ff6dc3205
Frank Thiemonge <fthiemonge@arrowstream.com>
2014-05-10 17:39:27 -0500

    Added bootstrap. Made minor page layout changes. Added placeholder for "Players" on Room page where the player's votes will be displayed.

--- AgilePoker/Models/Room.cs
+++ AgilePoker/Models/Room.cs
@@ -7,8 +7,8 @@ namespace AgilePoker.Models
     {
         public AgilePokerRoom PokerRoom { get; set; }
         public List<AgilePokerCard> PlayingCards { get; set; }
-        [Display(Name = "Select a card")]
         public decimal SelectedCardValue { get; set; }
+        [Display(Name="Select a Card")]
         public AgilePokerCard SelectedCard { get; set; }
     }
 }
